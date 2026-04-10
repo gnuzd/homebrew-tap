e5aeb834ab0f4ac385b4f3344f3034376cc4d601
Chris Nguyen <chris.dungnguyen@gmail.com>
2026-04-10 11:33:10 +0700

    fix: update atom v0.1.4 checksum

--- Formula/atom.rb
+++ Formula/atom.rb
@@ -2,7 +2,7 @@ class Atom < Formula
   desc "A lightning-fast, modal terminal editor written in Rust"
   homepage "https://github.com/gnuzd/atom"
   url "https://github.com/gnuzd/atom/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "477d76512a3100a4c0e6c5e3dd9b4eac905aeac100d93b04576fd2eea422d1a6"
+  sha256 "dcb924ad1866c8a4950a026b1712ed4b6d6213bb7e47474f07c52814a0241671"
   license "MIT"
 
   depends_on "rust" => :build
