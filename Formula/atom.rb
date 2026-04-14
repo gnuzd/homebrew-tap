7ecef6f0adcee0db451be1b2ca673b2bb925479a
Chris Nguyen <chris.dungnguyen@gmail.com>
2026-04-14 10:05:43 +0700

    feat: update version

--- Formula/atom.rb
+++ Formula/atom.rb
@@ -1,8 +1,8 @@
 class Atom < Formula
   desc "A lightning-fast, modal terminal editor written in Rust"
   homepage "https://github.com/gnuzd/atom"
-  url "https://github.com/gnuzd/atom/archive/refs/tags/v0.1.6.tar.gz"
-  sha256 "e936caad82306af3a0b5be9e6be738d56c8943f3e5e837053796f76281558683"
+  url "https://github.com/gnuzd/atom/archive/refs/tags/v0.17.tar.gz"
+  sha256 "ac8b6fa6898b14d58cf91b55a8d0bb7e9d997b67f5609d74d74a429a45d84eb5"
   license "MIT"
 
   depends_on "rust" => :build
