2f4874e9d548ecde19d2f0a7526b5cbfb2b0338d
Chris Nguyen <chris.dungnguyen@gmail.com>
2026-04-14 10:07:40 +0700

    fix: wrong verion

--- Formula/atom.rb
+++ Formula/atom.rb
@@ -1,7 +1,7 @@
 class Atom < Formula
   desc "A lightning-fast, modal terminal editor written in Rust"
   homepage "https://github.com/gnuzd/atom"
-  url "https://github.com/gnuzd/atom/archive/refs/tags/v0.17.tar.gz"
+  url "https://github.com/gnuzd/atom/archive/refs/tags/v0.1.7.tar.gz"
   sha256 "ac8b6fa6898b14d58cf91b55a8d0bb7e9d997b67f5609d74d74a429a45d84eb5"
   license "MIT"
 
