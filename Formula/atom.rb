3b39a667b935b4851d7656ae99fbe3b9790bc29a
Gnuzd <chris.dungnguyen@gmail.com>
2026-04-24 10:43:39 +0700

    feat: update atom to v0.1.11

--- Formula/atom.rb
+++ Formula/atom.rb
@@ -1,21 +1,21 @@
 class Atom < Formula
   desc "A lightning-fast, modal terminal editor written in Rust"
   homepage "https://github.com/gnuzd/atom"
-  version "0.1.10"
+  version "0.1.11"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/gnuzd/atom/releases/download/v0.1.10/atom-v0.1.10-aarch64-apple-darwin.tar.gz"
-      sha256 "8256129e0472bfe9afb8dd99c03d55e663e6145f5ea99ca0aaaa9a3a1b31b78b"
+      url "https://github.com/gnuzd/atom/releases/download/v0.1.11/atom-v0.1.11-aarch64-apple-darwin.tar.gz"
+      sha256 "c8bfc33f43404850349568e1e58335871483e2c2588c5015e31c623912489c16"
     else
-      url "https://github.com/gnuzd/atom/releases/download/v0.1.10/atom-v0.1.10-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/gnuzd/atom/releases/download/v0.1.11/atom-v0.1.11-x86_64-apple-darwin.tar.gz"
       sha256 "REPLACE_WITH_X86_64_SHA256"
     end
   end
 
   on_linux do
-    url "https://github.com/gnuzd/atom/archive/refs/tags/v0.1.10.tar.gz"
+    url "https://github.com/gnuzd/atom/archive/refs/tags/v0.1.11.tar.gz"
     sha256 "REPLACE_WITH_LINUX_SHA256"
     depends_on "rust" => :build
   end
